31bdcc4fc8eb4fcc2f103d800c62af7bb10234a1
as3mbus <asembus@gmail.com>
2024-04-19 12:10:48 +0700

    feat: add gpt 3.5 1106 and seed + response format json handling

--- Source/OpenAIAPI/Public/OpenAIDefinitions.h
+++ Source/OpenAIAPI/Public/OpenAIDefinitions.h
@@ -26,6 +26,7 @@ UENUM(BlueprintType)
 enum class EOAChatEngineType : uint8
 {
 	GPT_3_5_TURBO = 0 UMETA(ToolTip = "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration."),
+	GPT_3_5_TURBO_1106 = 4 UMETA(ToolTip = "GPT-3.5 Turbo model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens"),
 	GPT_4 = 1 UMETA(ToolTip = "More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat. Will be updated with our latest model iteration."),
 	GPT_4_32k = 2 UMETA(ToolTip = "Same capabilities as the base gpt-4 model but with 4x the context length. Will be updated with our latest model iteration."),
 	GPT_4_TURBO = 3 UMETA(ToolTip = "The latest GPT-4 model with improved instruction following, JSON mode, reproducible outputs, parallel function calling, and more. Returns a maximum of 4,096 output tokens. This preview model is not yet suited for production traffic. Learn more."),
@@ -235,6 +236,12 @@ struct FChatSettings
 
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
 	int32 maxTokens = 250;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+	int32 seed = 250;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "OpenAI")
+	bool useJsonFormat = false;
 };
 /*
 *Create speech
